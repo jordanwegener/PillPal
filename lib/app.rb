f585a54da5f01bb75930878cb5b72624ab8abeae
arbpotatoes <arbpotatoes@gmail.com>
2021-04-01 20:05:42 +1100

    Added method to process user input from menu but not working yet

--- lib/app.rb
+++ lib/app.rb
@@ -3,21 +3,50 @@ require "tty-prompt"
 require_relative ("med.rb")
 
 class App
+  def initialize
+    @prompt = TTY::Prompt.new(symbols: { marker: "→" })
+  end
+
   def run
-    clear
-    main_menu
+    loop do
+      clear
+      main_menu
+    end
   end
 
   def main_menu
     puts "-------------- PillPal --------------\n"
-    prompt = TTY::Prompt.new
-    prompt.select("Please choose from the following options.\n") do |menu|
+    input = @prompt.select("Please choose from the following options.\n\n") do |menu|
+      menu.help "(Choose using ↑/↓ arrow keys, press Enter to select)"
+      menu.show_help :always
       menu.choice "Add new medications", 1
       menu.choice "View, edit or delete existing medications", 2
       menu.choice "View and edit medication inventories and rebuy alerts", 3
       menu.choice "Get 3, 6 or 12 hour schedule", 4
       menu.choice "Get 1 week or 2 week schedule", 5
       menu.choice "Exit", 6
+      process_menu_input(input)
+    end
+  end
+
+  def process_menu_input(input)
+    case input
+    when 1
+      add_medication
+    when 2
+      medications_menu
+    when 3
+      inventory_menu
+    when 4
+      short_schedule_menu
+    when 5
+      long_schedule_menu
+    when 6
+      puts "Thanks for using PillPal!"
+      sleep(1)
+      exit
+    else
+      puts "Invalid selection" # REPLACE WITH PROPER ERROR HANDLING
     end
   end
 
