e3b7d001a064dad60befda2c588b9ec0d3684f0f
arbpotatoes <arbpotatoes@gmail.com>
2021-04-09 23:50:52 +1000

    Updated command line args

--- pillpal.rb
+++ pillpal.rb
@@ -5,28 +5,35 @@ require "tty-prompt"
 pillpal = App.new
 
 case ARGV[0]
-when "add"
+when "--add"
   pillpal.add_medication_menu(true)
-when "edit"
+when "--edit"
   pillpal.medications_menu(true)
-when "week"
+when "--week"
   pillpal.schedule_week(true)
-when "sched"
+when "--sched"
   pillpal.schedule_short(true)
-when "help"
+when "--help"
   puts "PillPal is a CLI app to help people remember and manage their medications."
   puts "\n"
   puts "Without arguments, the PillPal menu system will be displayed and all features can be used this way."
   puts "If you prefer, most functionality can be accessed using command line arguments."
   puts "\nArguments:"
-  puts "add       - add a new medication"
-  puts "edit      - show view/edit/delete medications menu"
-  puts "week      - print a 1 week schedule"
-  puts "sched     - print 3, 6, 12 or 24 hour schedules"
-  puts "help      - show help and command line arguments"
+  puts "--add       - add a new medication"
+  puts "--edit      - show view/edit/delete medications menu"
+  puts "--week      - print a 1 week schedule"
+  puts "--sched     - print 3, 6, 12 or 24 hour schedules"
+  puts "--help      - show help and command line arguments"
   exit
 else
   puts "Invalid argument."
+  puts "\nArguments:"
+  puts "--add       - add a new medication"
+  puts "--edit      - show view/edit/delete medications menu"
+  puts "--week      - print a 1 week schedule"
+  puts "--sched     - print 3, 6, 12 or 24 hour schedules"
+  puts "--help      - show help and command line arguments"
+  exit
 end
 
 pillpal.run
